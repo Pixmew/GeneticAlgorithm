ae0c4d547270814a12f5f3c38e71c995fb4645b7
yash <universeyash4@gmail.com>
2020-10-07 22:13:11 +0530

    added extra controls of pharase and added Cacualte fitness function for Dna Class

--- PhraseEvolver/PhraseViewer.java
+++ PhraseEvolver/PhraseViewer.java
@@ -1,8 +1,10 @@
 import java.util.concurrent.*;
 class PhaseViewer{
-
   public static void main(String[] args){
-    Population pop = new Population(20,5);
+    String targetPhrase = "To be or not to be.That is the Question!";
+    int targetLength = targetPhrase.length();
+    int population = 5;
+    Population pop = new Population(targetPhrase,targetLength,population);
   }
 }
 
@@ -11,10 +13,10 @@ class Population{
   Dna[] population;
 
 
-  Population(int length,int charlen){
+  Population(String target,int length,int numberOfPopulation){
     population = new Dna[length];
-    for (int i=0 ; i < length ; i++){
-      population[i] = new Dna(charlen);
+    for (int i=0 ; i < numberOfPopulation ; i++){
+      population[i] = new Dna(target,length);
     }
   }
 
@@ -24,15 +26,26 @@ class Population{
 
 class Dna{
 char[] phrase;
-
-
-  Dna(int length){
+int fitness = 0;
+String target;
+  Dna(String target,int length){
+    this.target = target;
     phrase = new char[length];
-    for(int i=0 ; i< phrase.length ; i++){
+    for(int i=0 ; i< this.phrase.length ; i++){
       phrase[i] = (char)ThreadLocalRandom.current().nextInt(65,122);
     }
     System.out.println(phrase);
+    this.CalcFitness();
+    System.out.println(fitness);
   }
 
-
+void CalcFitness(){
+    int count = 0;
+    for(int i =0 ; i < this.phrase.length ; i++){
+      if(this.phrase[i] == this.target.charAt(i)){
+        count++;
+      }
+    }
+    this.fitness = count;
+  }
 }
